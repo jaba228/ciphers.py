ce76e8b47bec127f23ca37279abd2009e0aacf5f
jaba228 <martin948173@gmail.com>
2024-03-03 12:58:18 +0200

    Added __pycache__ to .gitignore and writed algorithm for caesar cipher in ciphers.py

--- ciphers.py
+++ ciphers.py
@@ -1,2 +1,14 @@
-def caesar(message: str, step: int) -> str:
-    pass
+def caesar(message: str, step: int, alpha: str) -> str:
+    encrypted_message = ""
+
+    # TODO: add a choice for case sensitive/insensitive encryption
+    message = message.lower()
+    
+    for i, char in enumerate(message):
+        if char in alpha:
+            idx = alpha.find(char)
+            encrypted_message += alpha[(idx + step) % len(alpha)]
+        else:
+            encrypted_message += message[i]
+
+    return encrypted_message 
